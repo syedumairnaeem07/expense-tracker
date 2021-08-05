bc977047883c4fa41e8cb9d2b4693c2bce0be299
umair <sumairn.1997@gmail.com>
2021-08-05 17:15:06 +0500

    Adds IncomeExpenses Component

---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function IncomeExpenses() {
+	return (
+		<div class="inc-exp-container">
+			<div>
+				<h4>Income</h4>
+				<p id="money-plus" class="money plus">
+					+$0.00
+				</p>
+			</div>
+			<div>
+				<h4>Expense</h4>
+				<p id="money-minus" class="money minus">
+					-$0.00
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default IncomeExpenses;
